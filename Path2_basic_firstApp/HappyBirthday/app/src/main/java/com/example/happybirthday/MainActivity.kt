1a1499fa6df321c84c9192e6969b7a134675d1f5
VitorDLeo <vitordileo003@gmail.com>
2022-06-07 20:40:13 -0300

    First App using jetpack Compose

---
+++ Path2_basic_firstApp/HappyBirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -0,0 +1,73 @@
+package com.example.happybirthday
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.happybirthday.ui.theme.HappyBirthdayTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            HappyBirthdayTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    BirthdayGreetingWithImage(message = "Happy Birthday Sam!", from = "-from Emma")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun BirthdayGreetingWithImage(message: String, from: String){
+    val image = painterResource(id = R.drawable.androidparty)
+    Box{
+        Image(painter = image, contentDescription = null,
+            modifier = Modifier.fillMaxSize().fillMaxWidth(),
+            contentScale = ContentScale.Crop
+        )
+        BirthDayGreetingWithText(message = message, from = from)
+    }
+}
+
+@Composable
+fun BirthDayGreetingWithText(message : String, from : String){
+    Column {
+        Text(text = message,
+            fontSize = 36.sp,
+            modifier = Modifier.fillMaxWidth()
+                .wrapContentWidth(align = Alignment.Start)
+                .padding(start = 16.dp, top = 16.dp)
+        )
+        Text(text = from, fontSize = 24.sp,
+            modifier = Modifier.fillMaxWidth()
+                .wrapContentWidth(align = Alignment.End)
+                .padding(start = 16.dp, end = 16.dp)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BirthdayGreetingWithTextPreview(){
+    HappyBirthdayTheme {
+        BirthdayGreetingWithImage(message = "Happy Birthday Sam!", from = "-from Emma")
+    }
+}
